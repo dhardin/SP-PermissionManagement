1fed3d3c445d8419d4371569acd1ab69382a5833
dhardin <dhardin@ltu.edu>
2014-12-05 11:56:33 -0500

    function name correction

--- js/views/user.edit.js
+++ js/views/user.edit.js
@@ -95,7 +95,7 @@ app.UserEditView = Backbone.View.extend({
 	getUserPermissions: function(username){
 		app.data.getPermissions(app.config.url, username, function(results){
 			//format results
-			results = app.utility.processResults(results);
+			results = app.utility.procesData(results);
 			//publish results globally 
 			Backbone.pubSub.trigger('user:permissions-fetched', results);
 		});
