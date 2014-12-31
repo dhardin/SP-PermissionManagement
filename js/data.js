b0bf41dab715cf05dc59c46b1713885c15974bea
Dustin Hardin <dhardin07@gmail.com>
2014-12-30 22:48:16 -0500

    fixed web service call for GetUserCollectionFromGroup (attribute value incorrect)

--- js/data.js
+++ js/data.js
@@ -146,7 +146,7 @@ app.data = (function(){
          soapEnv =
             '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\
                 <soap:Body>\
-                  <GetUserCollectionFromGroup xmlns="http://schemas.microsoft.com/sharepoint/soap/" >\
+                   <GetUserCollectionFromGroup xmlns="http://schemas.microsoft.com/sharepoint/soap/directory/">\
                     <groupName>' + group + '</groupName>\
                   </GetUserCollectionFromGroup>\
               </soap:Body>\
