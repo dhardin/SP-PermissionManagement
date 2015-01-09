605746352ff1d1585edf325a827647a6c5517693
Dustin Hardin <dhardin07@gmail.com>
2015-01-09 12:17:31 -0500

    render optimzation

--- js/views/library.js
+++ js/views/library.js
@@ -1,68 +1,71 @@
 var app = app || {};
 
 app.LibraryView = Backbone.View.extend({
-	
-	initialize: function (options){
-		this.collection.on('add reset remove',function(){
-			this.render(this.collection);
-			}, this
-			);
 
-		this.itemView = options.itemView;
-		this.filter = options.filter || false;	
-		if (this.filter){
-			this.search(this.filter)
-		} else {
-			this.render();
-		}
-	},
+    initialize: function(options) {
+        this.collection.on('add reset remove', function() {
+            this.render(this.collection);
+        }, this);
 
-	render: function (collection, isFiltered) {
-		var numActiveItems = 0,
-		totalItems = 0,
-		numItemsDisplayed = 0;
-		collection = collection || this.collection;
+        this.itemView = options.itemView;
+        this.filter = options.filter || false;
+        if (this.filter) {
+            this.search(this.filter)
+        } else {
+            this.render();
+        }
+    },
 
-		this.$el.html('');
+    render: function(collection, isFiltered) {
+        var numActiveItems = 0,
+            totalItems = 0,
+            numItemsDisplayed = 0;
+        collection = collection || this.collection;
+        if (isFiltered && collection.length == this.collection.length) {
+            return this;
+        }
+        this.$el.html('');
 
-		if (!isFiltered){
-			if (collection.length > 0){
-				collection.each(function(item){	
-					this.renderItem(item);
-				}, this);
-			} else {
-				this.$el.html($('#noItemsTemplate').html());
-			}
-		} else {
-			//get the total number of active items
-			numActiveItems = this.collection.where({active: true}).length;
-			totalItems = numActiveItems;
-			numItemsDisplayed = collection.toArray().length;
-			if(numItemsDisplayed < totalItems){
-				this.$el.html('Displaying ' + numItemsDisplayed + ' out of ' + totalItems);
-			}
-			collection.each(function(item){
-				this.renderItem(item);
-			}, this);
-		}
-		
-		return this;
-			},
+        if (!isFiltered) {
+            if (collection.length > 0) {
+                collection.each(function(item) {
+                    this.renderItem(item);
+                }, this);
+            } else {
+                this.$el.html($('#noItemsTemplate').html());
+            }
+        } else {
+            //get the total number of active items
+            numActiveItems = this.collection.where({
+                active: true
+            }).length;
+            totalItems = numActiveItems;
+            numItemsDisplayed = collection.toArray().length;
+            if (numItemsDisplayed < totalItems) {
+                this.$el.html('Displaying ' + numItemsDisplayed + ' out of ' + totalItems);
+            }
+            collection.each(function(item) {
+                this.renderItem(item);
+            }, this);
+        }
 
-	renderItem: function(item){
-		var itemView = new this.itemView({
-			model: item
-		});
-		this.$el.append(itemView.render().el);
-	},
+        return this;
+    },
 
-	search: function(options){
-		var collection = (options && options.collection ? options.collection : this.collection);
+    renderItem: function(item) {
+        var itemView = new this.itemView({
+            model: item
+        });
+        this.$el.append(itemView.render().el);
+    },
 
-		if (!options){
-			this.render();
-		}else {
-			this.render(collection.search(options), true);	
-		}
-	}
+    search: function(options) {
+        var collection = (options && options.collection ? options.collection : this.collection);
+
+        if (!options) {
+            this.render();
+        } else {
+            this.render(collection.search(options), true);
+        }
+    }
 });
